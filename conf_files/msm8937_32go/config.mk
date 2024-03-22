0932325e3deebbc353c090894ebe54c0ca3bcaea
Sanjana Gogi <quic_sgogi@quicinc.com>
2024-03-22 10:33:25 +0530

    media: Disable qcom video service from configuration files for msm8937_32go
    Disabling qcom video acceleration for memory optimization.
    
    Change-Id: I105c0742042c84948aff1caa82cde3bf3e53cbed

--- conf_files/msm8937_32go/config.mk
+++ conf_files/msm8937_32go/config.mk
@@ -3,11 +3,18 @@ CONFIG_PATH := hardware/qcom/media/conf_files/msm8937_32go
 PRODUCT_COPY_FILES += $(CONFIG_PATH)/media_profiles_8937.xml:system/etc/media_profiles.xml \
                       $(CONFIG_PATH)/media_profiles_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
                       $(CONFIG_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-                      $(CONFIG_PATH)/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_8937_v1.xml \
-                      $(CONFIG_PATH)/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
+                      $(CONFIG_PATH)/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_8937_v1.xml 
+ifeq (($(TARGET_1G_DDR_RAM), true)
+        PRODUCT_COPY_FILES +=\ 
+                $(CONFIG_PATH)/media_codecs_vendor_msm8937_32go_1gb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml 
+else
+        PRODUCT_COPY_FILES +=\
+                      $(CONFIG_PATH)/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml 
+endif
+        PRODUCT_COPY_FILES +=\
                       $(CONFIG_PATH)/media_codecs_performance_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
                       $(CONFIG_PATH)/media_codecs_performance_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_vendor.xml \
-                      $(CONFIG_PATH)/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
+                      $(CONFIG_PATH)/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml 
 
 # Vendor property overrides
 ifeq ($(GENERIC_ODM_IMAGE),true)
